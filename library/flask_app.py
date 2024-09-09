90dc858435f5257ca759c249c9145a496f1bf66e
garvk <garv.khurana@fractal.ai>
2024-09-09 14:53:15 +0530

    Update flask_app.py
    increased limits

--- library/flask_app.py
+++ library/flask_app.py
@@ -65,7 +65,7 @@ app = Flask(__name__, static_folder=STATIC_DIR, template_folder=BUILD_DIR)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["5000 per day", "1000 per hour"],
     storage_uri="memory://",
 )
 
